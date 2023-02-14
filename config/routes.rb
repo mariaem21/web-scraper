3a5ce11a725bc5bb2e9416a8c9e5983dc884fc07
mariaem21 <mematamoros@icloud.com>
2023-02-13 18:10:46 -0600

    Removing an error that made the build fail part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-  root 'student_orgs#index'
   resources :student_orgs do
     match '/scrape', to: 'student_orgs#scrape', via: :post, on: :collection
   end
 
-  root to: 'student_orgs#index'
+  # root to: 'student_orgs#index'
+  root 'student_orgs#index'
 end
