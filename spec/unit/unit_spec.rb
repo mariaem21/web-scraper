cc602e4ca0988c282a2722c072d18d9f4c443a39
mariaem21 <mematamoros@icloud.com>
2023-02-20 16:39:42 -0600

    Wrote integration & unit tests

---
+++ spec/unit/unit_spec.rb
@@ -0,0 +1,57 @@
+# location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Organization, type: :model do
+    it "is not valid if organization PK is not unique" do
+        org1 = Organization.create(:orgID => 1, :name => "Student org A", :description => "Unique description")
+        org2 = Organization.create(:orgID => 1, :name => "Student org B", :description => "Different here")
+        expect(org1).to be_valid
+        expect(org2).to_not be_valid
+    end
+
+    it "is not valid if organization name is not unique" do
+        org1 = Organization.create(:orgID => 1, :name => "Student org A", :description => "Unique description")
+        org2 = Organization.create(:orgID => 2, :name => "Student org A", :description => "Different here")
+        expect(org1).to be_valid
+        expect(org2).to_not be_valid
+    end
+end
+
+RSpec.describe Application, type: :model do
+    it "is not valid if application PK is not unique" do
+        app1 = Application.create(:applicationID => 1, :orgID => 1, :name => "App A", :datebuilt => 20210621, :githublink => "https://gist.github.com/sseletskyy/5899820", :description => "Unique description")
+        app2 = Application.create(:applicationID => 1, :orgID => 2, :name => "App B", :datebuilt => 20210621, :githublink => "https://gist.github.com/sseletskyy/5899821", :description => "Different here")
+        expect(app1).to be_valid
+        expect(app2).to_not be_valid
+    end
+
+    it "is not valid if orgID is not valid" do
+        app1 = Application.create(:applicationID => 1, :orgID => -1, :name => "App A", :datebuilt => 20210621, :githublink => "https://gist.github.com/sseletskyy/5899820", :description => "Unique description")
+        app2 = Application.create(:applicationID => 1, :orgID => 5, :name => "App B", :datebuilt => 20210621, :githublink => "https://gist.github.com/sseletskyy/5899821", :description => "Different here")
+        expect(app1).to_not be_valid
+        expect(app2).to_not be_valid
+    end
+end
+
+RSpec.describe Contact, type: :model do
+    it "is not valid if contact PK is not unique" do
+        org1 = Organization.create(:orgID => 1, :name => "Student org A", :description => "Unique description")
+        person1 = Contact.create(:personID => 1, :orgID => 1, :year => 20210621, :name => "Person A", :email => "john@tamu.edu", :officerposition => "President", :description => "Unique description")
+        person2 = Contact.create(:personID => 1, :orgID => 1, :year => 20210621, :name => "Person B", :email => "johnsmith@tamu.edu", :officerposition => "Unknown", :description => "Different here")
+        expect(person1).to be_valid
+        expect(person2).to_not be_valid
+    end
+
+    it "is valid if orgID is not unique" do
+        org1 = Organization.create(:orgID => 1, :name => "Student org A", :description => "Unique description")
+        person1 = Contact.create(:personID => 1, :orgID => 1, :year => 20210621, :name => "Person A", :email => "john@tamu.edu", :officerposition => "President", :description => "Unique description")
+        person2 = Contact.create(:personID => 2, :orgID => 1, :year => 20210621, :name => "Person B", :email => "johnsmith@tamu.edu", :officerposition => "Unknown", :description => "Different here")
+        expect(person1).to be_valid
+        expect(person2).to be_valid
+    end
+
+    it "is not valid if no orgID is not valid" do
+        person1 = Contact.create(:personID => 1, :orgID => 1, :year => 20210621, :name => "Person B", :email => "johnsmith@tamu.edu", :officerposition => "Unknown", :description => "Different here")
+        expect(person1).to_not be_valid
+    end
+end
