5c7f0593d74a6e9a9be98fe6fbaf3a1571ecc34f
mariaem21 <mematamoros@icloud.com>
2023-02-20 08:43:05 -0600

    Added stuff to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+StudentOrg.create([{ name: "New student org", email: "john@tamn.edu", full_name: "John Smith"}])
