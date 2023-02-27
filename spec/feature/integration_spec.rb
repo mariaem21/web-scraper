3bdc93035f003a122cee6f4450b33bb7acf0a0cb
smarediya <sanamarediya@tamu.edu>
2023-02-27 17:17:38 -0600

    removed tests for filtering

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -47,23 +47,3 @@ RSpec.describe 'Application page for student org', type: :feature do
     expect(page).to have_content('2')
   end
 end
-
-RSpec.describe 'Filtering list of student orgs', type: :feature do
-
-    scenario 'Shows correct applications after applying filter' do
-        visit organizations_path 
-        click_on 'Scrape' 
-        visit organizations_appsbuilt_path 
-        click on 'filter >1'
-        expect(page).to have_content('No organizations match this fliter criteria') 
-    end
-
-    scenario 'Shows correct message when no orgs match filter criteria' do
-        visit organizations_path 
-        click_on 'Scrape' 
-        visit organizations_appsbuilt_path 
-        click on 'filter >5'
-        expect(page).to have_content('No organizations match this fliter criteria') 
-    end
-
-end
