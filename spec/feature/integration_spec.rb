229024b668f37265add51a8bb1aaf115091b7c9d
smarediya <sanamarediya@tamu.edu>
2023-02-27 17:15:49 -0600

    removed tests for view applications

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -8,46 +8,6 @@ RSpec.describe 'Scraping from STUACT', type: :feature do
     end
 end
 
-RSpec.describe 'Application page for student org', type: :feature do
-
-  scenario 'Shows correct message when no applications exist' do
-    visit abattery_applications_path
-    expect(page).to have_content('This organization does not have any applications')
-  end
-  
-  scenario 'Shows correct application once added' do
-    visit organizations_path
-    click_on 'Scrape'
-    visit organizations_abattery_path
-    click_on 'New application'
-    fill_in 'applicationID', with: '1'
-    fill_in 'orgID', with: '1'
-    fill_in 'name', with: 'Valid application'
-    fill_in 'datebuilt', with: '02/24/2023'
-    fill_in 'githublink', with: 'github.com'
-    fill_in 'description', with: 'First application test'
-    click_on 'Create application'
-    visit abattery_applications_name_path
-    expect(page).to have_content('Valid application')
-  end
-
-  scenario 'Shows correct organization name' do
-    visit abattery_applications_path
-    expect(page).to have_content('A Battery')
-  end
-
-  scenario 'Shows correct contact info' do
-    visit abattery_applications_contacts_path
-    expect(page).to have_content('Chad Parker')
-    expect(page).to have_content('cparker@corps.tamu.edu')
-  end
-
-  scenario 'Shows correct number of applications' do
-    visit abattery_applications_path
-    expect(page).to have_content('2')
-  end
-end
-
 RSpec.describe 'Filtering list of student orgs', type: :feature do
 
     scenario 'Shows correct applications after applying filter' do
