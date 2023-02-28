402023cea751a6b5e07e64b2df385bd1eeed6001
smarediya <sanamarediya@tamu.edu>
2023-02-27 19:13:01 -0600

    ran rubocop and corrected offenses

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,29 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'Scraping from STUACT', type: :feature do
-    scenario 'correct output' do
-        visit student_orgs_url
-        # click_on "Scrape"
-        # expect(page).to have_content('')
-    end
+RSpec.describe('Scraping from STUACT', type: :feature) do
+  it 'correct output' do
+    visit student_orgs_url
+    # click_on "Scrape"
+    # expect(page).to have_content('')
+  end
 end
 
-RSpec.describe 'Filtering list of student orgs', type: :feature do
+RSpec.describe('Filtering list of student orgs', type: :feature) do
+  it 'Shows correct applications after applying filter' do
+    visit organizations_path
+    click_on 'Scrape'
+    visit organizations_appsbuilt_path
+    click on('filter >1')
+    expect(page).to(have_content('No organizations match this fliter criteria'))
+  end
 
-    scenario 'Shows correct applications after applying filter' do
-        visit organizations_path 
-        click_on 'Scrape' 
-        visit organizations_appsbuilt_path 
-        click on 'filter >1'
-        expect(page).to have_content('No organizations match this fliter criteria') 
-    end
-
-    scenario 'Shows correct message when no orgs match filter criteria' do
-        visit organizations_path 
-        click_on 'Scrape' 
-        visit organizations_appsbuilt_path 
-        click on 'filter >5'
-        expect(page).to have_content('No organizations match this fliter criteria') 
-    end
-
+  it 'Shows correct message when no orgs match filter criteria' do
+    visit organizations_path
+    click_on 'Scrape'
+    visit organizations_appsbuilt_path
+    click on('filter >5')
+    expect(page).to(have_content('No organizations match this fliter criteria'))
+  end
 end
