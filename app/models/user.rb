290d11906d98d6460ed0e70b9dd2f24fea08efe8
mariaem21 <mematamoros@icloud.com>
2023-03-01 10:20:11 -0600

    Added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :userID, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :isadmin, presence: true
 end
