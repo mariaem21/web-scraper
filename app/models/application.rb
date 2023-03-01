c5fa62e0f5a44db74256303173b1511ee9c32152
mariaem21 <mematamoros@icloud.com>
2023-03-01 10:06:38 -0600

    Added validation for application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,21 @@
 class Application < ApplicationRecord
+    validates :applicationID, presence: true, uniqueness: true
+    validates :orgID, presence: true
+    validates :name, presence: true
+    validates :datebuilt, presence: true
+    validates :githublink, presence: true
+    validates :description, presence: true
+    
+    # has_one :contact
+    # validates :contact, presence: true
+    # belongs_to :organization
+    validate :orgID_exists
+    has_many :appcats
+    has_many :categories, through: :appcats
+
+    def orgID_exists
+        if !Organization.where(orgID: self.orgID).exists? then
+            errors.add(:orgID, 'Must have a valid organization ID.')
+        end
+    end
 end
