b4ab3c5560f6e76f8997fc945ce868e814a919c7
Sai Juttu <43052053+saijuttu@users.noreply.github.com>
2023-03-02 00:00:51 -0600

    testing test cases

--- spec/system/delete_applications_spec.rb
+++ spec/system/delete_applications_spec.rb
@@ -1,18 +1,46 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "DeleteApplications", type: :system do
-  before do
-    driven_by(:rack_test)
+RSpec.describe 'DeleteApplications', type: :system do
+  
+#     before do
+#     driven_by(:rack_test)
+#   end
+
+  before(:all) do
+    Capybara.current_driver = :selenium
   end
 
+
   it 'deletes application successfully' do
-    visit '/applications'
+    
 
-    click_on 'delete'
+    visit applications_path
 
-    click_on 'confirm'
+    click_on 'New application'
+    fill_in "application[name]", with: 'Test Application'
+    fill_in "application[orgID]", with: '01'
+    fill_in "application[applicationID]", with: '01'
+    click_on 'Create Application'
 
-    expect(page).to have_content('Application Deleted!')
+    click_on 'Destroy this application'
+
+    accept_confirm do
+        click_link 'OK'
+      end
+    # page.driver.browser.accept_js_confirms
+    # dialog = page.driver.browser.switch_to.alert
+    # dialog.accept
+
+    expect(page).to have_content('Application was successfully destroyed.')
+    
+    
+    
+  end
+
+  after(:all) do
+    Capybara.use_default_driver
   end
 
 end
