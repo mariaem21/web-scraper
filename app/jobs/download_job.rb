8c2194b05e24780a1eaea24211f6e56e41beafb0
mariaem21 <mematamoros@icloud.com>
2023-03-02 00:41:51 -0600

    Passing basic CSV download tests

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -9,7 +9,7 @@ class DownloadJob < ApplicationJob
         orgTable = Organization.all;
 
         CSV.open("#{filename}.csv", "w") do |csv|
-            csv << ["ID", "Student Organization", "Contact Name", "Contact Email", "Officer Position", "Last Modified"]
+            csv << ["ID", "Student Organization", "Contact Name", "Contact Email", "Officer Position", "Last Modified", "Number of Apps Built"]
             
             # Loop through all organizations and add to CSV
             orgTable.each_with_index do |org, index|
@@ -19,6 +19,7 @@ class DownloadJob < ApplicationJob
                 contactEmail = "Not provided on STUACT website"
                 officerPosition = "Not provided on STUACT website"
                 updateYear = "Contact information was never entered"
+                appsBuilt = 0
 
                 # Find contact associated with student org
                 if Contact.where(orgID: orgid).exists? then #There is a contact attributed to org, add to csv
@@ -28,7 +29,7 @@ class DownloadJob < ApplicationJob
                     updateYear = Contact.find_by(orgID: org.orgID).year #Last updated in database
                 end
 
-                csv << [orgid, orgName, contactName, contactEmail, officerPosition, updateYear]
+                csv << [orgid, orgName, contactName, contactEmail, officerPosition, updateYear, appsBuilt]
             end
         end
     end
