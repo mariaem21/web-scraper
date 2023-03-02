21b27984368f651ce4cf42e54f66357742b0ce1f
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-03-02 00:15:42 -0600

    Changed applicationID match unit test case

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe Application, type: :model do
 
     app01 = Application.create(applicationID: 1, orgID: 0, name: 'Test App 0', datebuilt: 20_210_621,
                                githublink: 'github.com', description: 'test app 0_1')
-    app02 = Application.create(applicationID: 9, orgID: 0, name: 'Test App 1', datebuilt: 20_210_621,
+    app02 = Application.create(applicationID: 1, orgID: 0, name: 'Test App 1', datebuilt: 20_210_621,
                                githublink: 'github.com', description: 'test app 0_2')
 
     expect(app01).to be_valid
