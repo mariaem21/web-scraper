18bdbe9b0fa53cb3f7daa0ca9f856287ee080540
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-03-02 22:30:18 -0600

    Commented validate function lines at the top causing errors from commenting of function definitions

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,9 @@
 class Application < ApplicationRecord
   validates :applicationID, presence: true, uniqueness: true, comparison: { greater_than: 0 }
-  validate :if_applicationid_negative
+  # validate :if_applicationid_negative
   validates :orgID, presence: true, comparison: { greater_than: 0 }
   validate :orgid_exists
-  validate :if_orgid_negative
+  # validate :if_orgid_negative
   validates :name, presence: true
   validates :datebuilt, presence: true
   validates :githublink, presence: true
