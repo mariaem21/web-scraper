8c71637aa46064c12b1376d4ef7bb26269283ab3
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-03-02 20:55:58 -0600

    Changed orgid and applicationid negative to check with 0 and removed unnecessary test case

--- app/models/application.rb
+++ app/models/application.rb
@@ -22,7 +22,7 @@ class Application < ApplicationRecord
 
   def if_orgid_negative
     if !:orgID.blank?
-      if Application.where('orgID < ?', 1)
+      if Application.where('orgID < ?', 0)
         errors.add(:orgID, 'cannot be less than 1')
       end
     end
@@ -30,7 +30,7 @@ class Application < ApplicationRecord
 
   def if_applicationid_negative
     if !:applicationID.blank?
-      if Application.where('applicationID < ?', 1)
+      if Application.where('applicationID < ?', 0)
         errors.add(:applicationID, 'cannot be less than 1')
       end
     end
