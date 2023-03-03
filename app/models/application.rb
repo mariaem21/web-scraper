3957ec2aacca35fb5b298deb32831e9a7266c67d
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-03-02 22:48:05 -0600

    Combining my test cases and new code

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class Application < ApplicationRecord
-    validates :applicationID, presence: true, uniqueness: true
-    validates :orgID, presence: true
+    validates :applicationID, presence: true, uniqueness: true, comparison: { greater_than: 0 }
+    validates :orgID, presence: true, comparison: { greater_than: 0 }
+    validate :orgID_exists
     validates :name, presence: true
     validates :datebuilt, presence: true
     validates :githublink, presence: true
     validates :description, presence: true
-    
     # has_one :contact
     # validates :contact, presence: true
     # belongs_to :organization
-    validate :orgID_exists
     has_many :appcats
     has_many :categories, through: :appcats
 
