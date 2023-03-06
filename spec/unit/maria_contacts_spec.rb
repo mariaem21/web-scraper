e156682e45fa821f124cde7297d27dfe921bad9c
mariaem21 <mematamoros@icloud.com>
2023-03-06 16:57:32 -0600

    Finished unit tests for contact

--- spec/unit/maria_contacts_spec.rb
+++ spec/unit/maria_contacts_spec.rb
@@ -21,4 +21,32 @@ RSpec.describe Contact, type: :model do
                                email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: 'Different here')
       expect(person1).to_not be_valid
     end
+
+    it 'is not valid if any field besides is blank' do
+      org1 = Organization.create(organization_id: 1, name: 'Testing contact table', description: 'Unique description')
+      person1 = Contact.create(contact_id: '', organization_id: 1, year: 20_210_621, name: 'Person A',
+                               email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: 'Different here')
+      person2 = Contact.create(contact_id: 1, organization_id: '', year: 20_210_621, name: 'Person B',
+                               email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: 'Different here')
+      person3 = Contact.create(contact_id: 2, organization_id: 1, year: '', name: 'Person C',
+                               email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: 'Different here')
+      person4 = Contact.create(contact_id: 3, organization_id: 1, year: 20_210_621, name: '',
+                               email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: 'Different here')
+      person5 = Contact.create(contact_id: 4, organization_id: 1, year: 20_210_621, name: 'Person D',
+                               email: '', officer_position: 'Unknown', description: 'Different here')    
+      person6 = Contact.create(contact_id: 5, organization_id: 1, year: 20_210_621, name: 'Person E',
+                               email: 'johnsmith@tamu.edu', officer_position: '', description: 'Different here')
+      person7 = Contact.create(contact_id: 6, organization_id: 1, year: 20_210_621, name: 'Person F',
+                               email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: '')  
+      person8 = Contact.create(contact_id: 7, organization_id: 1, year: 20_210_621, name: 'Person G',
+                               email: 'johnsmith@tamu.edu', officer_position: 'Unknown', description: 'Valid contact')                                         
+      expect(person1).to_not be_valid
+      expect(person2).to_not be_valid
+      expect(person3).to_not be_valid
+      expect(person4).to_not be_valid
+      expect(person5).to_not be_valid
+      expect(person6).to_not be_valid
+      expect(person7).to_not be_valid
+      expect(person8).to be_valid
+    end
 end
