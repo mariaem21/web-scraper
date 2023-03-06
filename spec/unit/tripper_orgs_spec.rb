5dc94dd0cd2a20d50e117afbe8449d99f95f597f
mariaem21 <mematamoros@icloud.com>
2023-03-06 15:24:12 -0600

    Fixed syntax error

--- spec/unit/tripper_orgs_spec.rb
+++ spec/unit/tripper_orgs_spec.rb
@@ -21,23 +21,23 @@ RSpec.describe Organization, type: :model do
     end
 
     it 'is not valid if orgID is not valid/does not exist' do
-      org1 = Organization.create(orgID: 1, name: 'Student org A', description: 'Unique description')
+      org1 = Organization.create(organization_id: 1, name: 'Student org A', description: 'Unique description')
       org2 = Organization.create(name: 'Student org B', description: 'Different here')
       expect(org1).to be_valid
       expect(org2).to_not be_valid
     end
 
     it 'is not valid if name is empty' do
-      org1 = Organization.create(orgID: 1, name: 'Student org A', description: 'Unique description')
-      org2 = Organization.create(orgID: 2, name: '', description: 'Different here')
+      org1 = Organization.create(organization_id: 1, name: 'Student org A', description: 'Unique description')
+      org2 = Organization.create(organization_id: 2, name: '', description: 'Different here')
       expect(org1).to be_valid
       expect(org2).to_not be_valid
     end
 
   
     it 'is not valid if description is empty' do
-      org1 = Organization.create(orgID: 1, name: 'Student org A', description: 'Unique description')
-      org2 = Organization.create(orgID: 2, name: 'Student org B', description: '')
+      org1 = Organization.create(organization_id: 1, name: 'Student org A', description: 'Unique description')
+      org2 = Organization.create(organization_id: 2, name: 'Student org B', description: '')
       expect(org1).to be_valid
       expect(org2).to_not be_valid
     end
