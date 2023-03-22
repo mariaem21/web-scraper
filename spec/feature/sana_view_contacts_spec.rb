f194e5ea67886e25af31fe8728e367cfa777022d
smarediya <sanamarediya@tamu.edu>
2023-03-21 21:34:46 -0500

    started on tests

---
+++ spec/feature/sana_view_contacts_spec.rb
@@ -0,0 +1,49 @@
+# By: Sana
+
+# frozen_string_literal: true
+# location: spec/feature/feature_spec.rb
+require 'rails_helper'
+
+RSpec.describe('Application page for student org', type: :feature) do
+
+  # it 'Shows correct message when no applications exist' do
+  #   visit applications_path
+  #   expect(page).to(have_content('This organization does not have any applications'))
+  # end
+
+  scenario 'Shows correct application once added' do
+    org = Organization.create(organization_id: 1, name: 'A Battery', description: 'description')
+    app1 = Application.create(application_id: 1, organization_id: 1, name: 'first application', date_built:20_210_621, github_link: 'github.com', description: 'test')
+    visit applications_path
+    expect(Application.find_by(application_id: 1).name).to eq('first application')
+    expect(Organization.find_by(organization_id: 1).name).to eq('A Battery')
+
+    visit organizations_path
+    click_on "Show this organization"
+    click_on "Destroy this organization"
+    visit applications_path
+    click_on "Show this application"
+    click_on "Destroy this application"
+  end
+
+  scenario 'Shows correct contact info on org page' do
+    org = Organization.create(organization_id: 1, name: 'A Battery', description: 'description')
+    contact = Contact.create(contact_id: 1, organization_id: 1, year: 20_210_621, name: 'Chad Parker',
+                             email: 'cparker@corps.tamu.edu', officer_position: 'President', description: 'Unique description')
+    visit contacts_path
+    expect(Contact.find_by(contact_id: 1).name).to eq('Chad Parker')
+    expect(Contact.find_by(contact_id: 1).email).to eq('cparker@corps.tamu.edu')
+  end
+
+
+end
+
+# correct contact on org page
+# correct contact on 1 application
+# correct contacts with multiple applications
+
+# contact a (on org page)
+# app a = contact a
+# contact b
+# app b = contact b
+# app c = contact a
