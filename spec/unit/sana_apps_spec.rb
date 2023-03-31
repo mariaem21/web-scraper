b6b9d8a449201c9c78d1c8897c1ce03f00e1e417
smarediya <sanamarediya@tamu.edu>
2023-03-31 00:17:27 -0500

    Fixed some old tests

--- spec/unit/sana_apps_spec.rb
+++ spec/unit/sana_apps_spec.rb
@@ -18,26 +18,28 @@ RSpec.describe Application, type: :model do
       expect(app2).to_not be_valid
     end
   
-    it 'is not valid if application ID is not valid/does not exist' do
+    it 'is not valid if application ID is not valid' do
       app1 = Application.create(application_id: -1, contact_organization_id: 1, name: 'App 1', date_built: 20_210_621,
                                 github_link: 'link2', description: 'description')
       expect(app1).to_not be_valid
     end
 
     # it 'delete application' do
-    #   app = Application.find(application_id: 1).destroy()
+    #   app1 = Application.create(application_id: 1, contact_organization_id: 1, name: 'App A', date_built: 20_210_621,
+    #                             github_link: 'https://gist.github.com/sseletskyy/5899820', description: 'Unique description')
+    #   Application.find(application_id: 1).destroy()
     #   expect(Applications.find(application_id: 1).empty?).to be_valid
     # end
   
-    # it 'is not valid if orgID is not valid/does not exist' do
-    #   app3 = Application.create(application_id: 2, organization_id: 2, name: 'Test App 3', date_built: 20_210_621,
-    #                             github_link: 'github.com', description: 'test app 3')
-    #   app4 = Application.create(application_id: 4, organization_id: -2, name: 'Test App 4', date_built: 20_210_621,
-    #                             github_link: 'github.com', description: 'test app 4')
+    it 'is not valid if contact org ID is not valid/does not exist' do
+      app3 = Application.create(application_id: 2, contact_organization_id: 2, name: 'Test App 3', date_built: 20_210_621,
+                                github_link: 'github.com', description: 'test app 3')
+      app4 = Application.create(application_id: 4, contact_organization_id: -2, name: 'Test App 4', date_built: 20_210_621,
+                                github_link: 'github.com', description: 'test app 4')
   
-    #   expect(app3).to_not be_valid
-    #   expect(app4).to_not be_valid
-    # end
+      # expect(app3).to_not be_valid
+      expect(app4).to_not be_valid
+    end
 
     it 'is not valid if name is empty' do
       app5 = Application.create(application_id: 5, contact_organization_id: 0, name: '', date_built: 20_210_621,
