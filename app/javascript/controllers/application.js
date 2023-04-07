a3f7f0b1146f17f86715bd8cafcba6fa5c8143c0
mariaem21 <mematamoros@icloud.com>
2023-04-07 11:02:00 -0500

    Fixed test... sorry for breaking it

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
-window.Stimulus = application
 
 // Configure Stimulus development experience
 application.debug = false
