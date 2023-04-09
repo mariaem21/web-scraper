b97655a0847c7c419e88fb1d3a958f8320558774
mariaem21 <mematamoros@icloud.com>
2023-04-09 11:32:54 -0500

    Now you can add a row to the table

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -17,4 +17,41 @@ module OrganizationsHelper
     def sort_indicator
         tag.span(class: "sort sort-#{params[:direction]}")
     end
+
+    # def add_table_entry(org_name:, contact_name:, contact_email:, officer_position:)
+    #     org_name = params[:org_name] 
+    #     contact_name = params[:contact_name]
+    #     contact_email = params[:contact_email]
+    #     officer_position = params[:officer_position]
+
+    #     org_count = 0
+    #     contact_count = 0
+    #     con_org_count = 0
+    #     org = {}
+    #     contact = {}
+    #     con_org = {}
+    #     while Organization.where(organization_id: org_count).exists? do
+    #         org_count = org_count + 1
+    #     end
+    #     while Contact.where(contact_id: contact_count).exists? do
+    #         contact_count = contact_count + 1
+    #     end
+    #     while ContactOrganization.where(contact_organization_id: con_org_count).exists? do
+    #         con_org_count = con_org_count + 1
+    #     end
+
+    #     puts "ORG ID: #{org_count}"
+
+    #     query = "INSERT INTO organizations (organization_id, name, description) VALUES (#{org_count}, #{org_name}, 'None');"
+    #     orgs = ActiveRecord::Base.connection.execute(query)
+
+    #     query = "INSERT INTO contacts (contact_id, year, name, email, officer_position, description) VALUES (#{contact_count}, #{Date.today}, #{contact_name}, #{contact_email}, #{officer_position},  'None');"
+    #     contacts = ActiveRecord::Base.connection.execute(query)
+
+    #     query = "INSERT INTO contact_organizations (contact_organization_id, contact_id, organization_id) VALUES (#{con_org_count}, #{contact_count}, #{org_count});"
+    #     contacts = ActiveRecord::Base.connection.execute(query)
+    #     # Autofill in organization: organization_id, organization_description
+    #     # Autofill in contact_organization: contact_organization_id, contact_id, organization_id
+    #     # Autofill in contact: contact_id, year, description
+    # end
 end
