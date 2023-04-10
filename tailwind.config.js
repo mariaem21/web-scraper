71506b3516ad375630d2a943779ea8f088272087
TripperW <86893841+TripperW@users.noreply.github.com>
2023-04-10 14:41:00 -0500

    org view fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,9 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/*.css',
   ],  
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-    },
-  },
   safelist: [
     {
       pattern: /./,
